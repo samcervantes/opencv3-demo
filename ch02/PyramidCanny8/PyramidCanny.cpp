66d4b0ba35902fe12321d464872b5f152f0a28cd
samcervantes <cervantes.samuel@gmail.com>
2018-01-31 20:49:06 -0500

    add PyramidCanny

--- ch02/PyramidCanny8/PyramidCanny.cpp
+++ ch02/PyramidCanny8/PyramidCanny.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
 	
-	cv::Mat img_rgb, img_gry, img_cny;
+	cv::Mat img_rgb, img_gry, img_cny, img_pyr, img_pyr2;
 
 	cv::namedWindow("Example Gray", cv::WINDOW_AUTOSIZE);
 	cv::namedWindow("Example Canny", cv::WINDOW_AUTOSIZE);
@@ -12,7 +12,9 @@ int main(int argc, char** argv) {
 	cv::cvtColor(img_rgb, img_gry, cv::COLOR_BGR2GRAY);
 	cv::imshow("Example Gray", img_gry);
 
-	cv::Canny(img_gry, img_cny, 10, 100, 3, true);
+	cv::pyrDown(img_gry, img_pyr);
+	cv::pyrDown(img_pyr, img_pyr2);
+	cv::Canny(img_pyr2, img_cny, 10, 100, 3, true);
 
 	cv::imshow("Example Canny", img_cny);
 
