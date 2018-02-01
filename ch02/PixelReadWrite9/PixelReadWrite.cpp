af1b390a4d0bc135c3771abba408298f740cd40d
samcervantes <cervantes.samuel@gmail.com>
2018-02-01 17:21:39 -0500

    add WIP PixelReadWrite9

---
+++ ch02/PixelReadWrite9/PixelReadWrite.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv) {
+	
+	cv::Mat img_rgb, img_gry, img_cny, img_pyr, img_pyr2;
+
+	cv::namedWindow("Example Gray", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Example Canny", cv::WINDOW_AUTOSIZE);
+
+	img_rgb = cv::imread(argv[1]);
+
+	cv::cvtColor(img_rgb, img_gry, cv::COLOR_BGR2GRAY);
+	cv::imshow("Example Gray", img_gry);
+
+	cv::pyrDown(img_gry, img_pyr);
+	cv::pyrDown(img_pyr, img_pyr2);
+	cv::Canny(img_pyr2, img_cny, 10, 100, 3, true);
+
+	cv::imshow("Example Canny", img_cny);
+
+	int x = 16, y = 32;
+	cv::Vec3b intensity = img_rgb.at< cv::Vec3b >(y, x);
+
+	// Note we could write img_rgb.at
+
+
+
+	cv::waitKey(0);
+
+}
